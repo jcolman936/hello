1b7a9559b0f814a14394372e07f5ac8fb5db6fed
jcolman936 <jcolman936@gmail.com>
2025-08-25 13:06:22 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+ int main(int argc, car **argv) {
+
+    printf("Hello, I'm Jaedyn Colman!\n");
+    printf("Major is Computer Science!\n");
+
+    return 0;
+ }
