5526e42b1ea9852eea26acb384c2ca072eda7c08
evadde2 <evadde2@huskers.unl.edu>
2025-08-29 11:33:57 -0500

    hello

---
+++ hello.c
@@ -0,0 +1,19 @@
+/*
+Esha Vadde
+evadde2@huskers.unl.edu
+
+2025-08-28
+
+
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Hello World, Esha Vadde!\n");
+    printf("My major is Computer Science!\n");
+    printf("I like taking walks\n");
+
+    return 0;
+}
